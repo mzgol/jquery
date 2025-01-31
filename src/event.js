02ec13e5f9b740d735e180a6fbf5bc4ddad92846
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2025-01-31 01:09:34 +0100

    Event: Extract leverageNative to a separate module

--- src/event.js
+++ src/event.js
@@ -1,25 +1,19 @@
 import { jQuery } from "./core.js";
+import { returnTrue } from "./var/returnTrue.js";
+import { returnFalse } from "./var/returnFalse.js";
 import { documentElement } from "./var/documentElement.js";
 import { rnothtmlwhite } from "./var/rnothtmlwhite.js";
 import { rcheckableType } from "./var/rcheckableType.js";
-import { slice } from "./var/slice.js";
 import { isIE } from "./var/isIE.js";
 import { acceptData } from "./data/var/acceptData.js";
 import { dataPriv } from "./data/var/dataPriv.js";
 import { nodeName } from "./core/nodeName.js";
+import { leverageNative } from "./event/leverageNative.js";
 
 import "./core/init.js";
 import "./selector.js";
 
 var rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
-
-function returnTrue() {
-	return true;
-}
-
-function returnFalse() {
-	return false;
-}
 
 function on( elem, types, selector, data, fn, one ) {
 	var origFn, type;
@@ -496,110 +490,6 @@ jQuery.event = {
 		}
 	} )
 };
-
-// Ensure the presence of an event listener that handles manually-triggered
-// synthetic events by interrupting progress until reinvoked in response to
-// *native* events that it fires directly, ensuring that state changes have
-// already occurred before other listeners are invoked.
-function leverageNative( el, type, isSetup ) {
-
-	// Missing `isSetup` indicates a trigger call, which must force setup through jQuery.event.add
-	if ( !isSetup ) {
-		if ( dataPriv.get( el, type ) === undefined ) {
-			jQuery.event.add( el, type, returnTrue );
-		}
-		return;
-	}
-
-	// Register the controller as a special universal handler for all event namespaces
-	dataPriv.set( el, type, false );
-	jQuery.event.add( el, type, {
-		namespace: false,
-		handler: function( event ) {
-			var result,
-				saved = dataPriv.get( this, type );
-
-			// This controller function is invoked under multiple circumstances,
-			// differentiated by the stored value in `saved`:
-			// 1. For an outer synthetic `.trigger()`ed event (detected by
-			//    `event.isTrigger & 1` and non-array `saved`), it records arguments
-			//    as an array and fires an [inner] native event to prompt state
-			//    changes that should be observed by registered listeners (such as
-			//    checkbox toggling and focus updating), then clears the stored value.
-			// 2. For an [inner] native event (detected by `saved` being
-			//    an array), it triggers an inner synthetic event, records the
-			//    result, and preempts propagation to further jQuery listeners.
-			// 3. For an inner synthetic event (detected by `event.isTrigger & 1` and
-			//    array `saved`), it prevents double-propagation of surrogate events
-			//    but otherwise allows everything to proceed (particularly including
-			//    further listeners).
-			// Possible `saved` data shapes: `[...], `{ value }`, `false`.
-			if ( ( event.isTrigger & 1 ) && this[ type ] ) {
-
-				// Interrupt processing of the outer synthetic .trigger()ed event
-				if ( !saved.length ) {
-
-					// Store arguments for use when handling the inner native event
-					// There will always be at least one argument (an event object),
-					// so this array will not be confused with a leftover capture object.
-					saved = slice.call( arguments );
-					dataPriv.set( this, type, saved );
-
-					// Trigger the native event and capture its result
-					this[ type ]();
-					result = dataPriv.get( this, type );
-					dataPriv.set( this, type, false );
-
-					if ( saved !== result ) {
-
-						// Cancel the outer synthetic event
-						event.stopImmediatePropagation();
-						event.preventDefault();
-
-						// Support: Chrome 86+
-						// In Chrome, if an element having a focusout handler is
-						// blurred by clicking outside of it, it invokes the handler
-						// synchronously. If that handler calls `.remove()` on
-						// the element, the data is cleared, leaving `result`
-						// undefined. We need to guard against this.
-						return result && result.value;
-					}
-
-				// If this is an inner synthetic event for an event with a bubbling
-				// surrogate (focus or blur), assume that the surrogate already
-				// propagated from triggering the native event and prevent that
-				// from happening again here.
-				} else if ( ( jQuery.event.special[ type ] || {} ).delegateType ) {
-					event.stopPropagation();
-				}
-
-			// If this is a native event triggered above, everything is now in order.
-			// Fire an inner synthetic event with the original arguments.
-			} else if ( saved.length ) {
-
-				// ...and capture the result
-				dataPriv.set( this, type, {
-					value: jQuery.event.trigger(
-						saved[ 0 ],
-						saved.slice( 1 ),
-						this
-					)
-				} );
-
-				// Abort handling of the native event by all jQuery handlers while allowing
-				// native handlers on the same element to run. On target, this is achieved
-				// by stopping immediate propagation just on the jQuery event. However,
-				// the native event is re-wrapped by a jQuery one on each level of the
-				// propagation so the only way to stop it for jQuery is to stop it for
-				// everyone via native `stopPropagation()`. This is not a problem for
-				// focus/blur which don't bubble, but it does also stop click on checkboxes
-				// and radios. We accept this limitation.
-				event.stopPropagation();
-				event.isImmediatePropagationStopped = returnTrue;
-			}
-		}
-	} );
-}
 
 jQuery.removeEvent = function( elem, type, handle ) {
 
